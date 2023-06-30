15e9cf67855f5299a75ac8b89fcab0936dcdfe7b
Lucas <107816056+LucasSGonza@users.noreply.github.com>
2023-06-30 16:20:33 -0300

    Add files via upload

--- aulasAPI/APIPokemon/index.js
+++ aulasAPI/APIPokemon/index.js
@@ -1,35 +1,45 @@
 //https://pokeapi.co/
 
-function getPokemonName(){
+function getPokemonName() {
     var pokemonName = document.getElementById("input-pokemon").value
     console.log(pokemonName)
     return pokemonName
 }
 
 //func consultar api
-function searchPokemonInfos(){
+function searchPokemonInfos() {
     let pokemonName = getPokemonName()
     var url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
     fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        //console.log(data)
-        document.getElementById("div-img-pokemon").innerHTML = `<img src="${data.sprites.front_default}">`
-        document.getElementById("div-infos-pokemon").innerHTML =
+        .then(res => res.json())
+        .then(data => {
+            //console.log(data)
+            document.getElementById("div-img-pokemon").innerHTML = `<img src="${data.sprites.front_default}">`
+            document.getElementById("div-infos-pokemon").innerHTML =
                 `
                 <p> 
                 Name: ${data.name} <br>
                 Type: ${data.types[0].type.name}
                 </p>
                 `
-        whatBackground(data.types[0].type.name)
-    })
+            defineBackground(data.types[0].type.name, verifySecondType(data.types[1].type.name))
+        })
 }
 
-function whatBackground(pokemonType) {
+function defineBackground(pokemonType, pokemonType2) {
     var divImgPokemon = document.getElementById("div-img-pokemon");
-    console.log(pokemonType)
-    divImgPokemon.style.backgroundImage = `url(img/${pokemonType}.jpg)`;
-    //divImgPokemon.style.backgroundImage = pokemonType === "normal" && data.types[1] ? `url(img/${pokemonType}.avif)` : ;
-    
+    if (pokemonType2 != "fodase") {
+        divImgPokemon.style.backgroundImage = `url(img/${pokemonType2}.jpg)`;
+    } else {
+        divImgPokemon.style.backgroundImage = `url(img/${pokemonType}.jpg)`;
+    }
+    console.log(pokemonType, pokemonType2)
+}
+
+function verifySecondType(pokemonType2) {
+    if (pokemonType2 != null) {
+        return pokemonType2
+    } else {
+        return "fodase"
+    }
 }
